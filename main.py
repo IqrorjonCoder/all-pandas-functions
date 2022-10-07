33b29a3ae30e8cf6d0bc04e021fa8639a20dedac
Iqrorjon Islomov <111178357+IqrorjonCoder@users.noreply.github.com>
2022-10-07 16:49:27 +0500

    all pandas functions

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+
+functions = [i for i in dir(pd) if i[:1] != '_']
+
+for i, v in enumerate(functions):
+    print(i, v)
+
+    os.mkdir(f"./{i} {v}")
+    with open(f"././{i} {v}/{v.lower()}.ipynb", 'w') as f:
+        pass
+
+
+
+# for i,v in enumerate(dir(pd.read_csv)):
+#     print(i, v)
