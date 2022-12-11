26e79d7b09b54b3091f0b24bbf436a165d9966b4
Iqrorjon Islomov <111178357+IqrorjonCoder@users.noreply.github.com>
2022-12-11 08:46:27 -0500

    Delete rename.py

--- rename.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df1 = pd.DataFrame({'a': [1,2,3,None],
-                    'b': [4,5,6,None]})
-
-print(df1)
-print(df1.rename(columns={'a': 'A', 'b': 'B'}))
-print(df1.rename(index={0:'zero', 1:'one', 2:'two', 3:'three'}))
